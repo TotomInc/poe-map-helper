53ae263bbd941b2322747c1839798f41047f591d
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-18 16:51:32 +0200

    refacto(main): modularize background into routine

---
+++ src/main/routine.ts
@@ -0,0 +1,69 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { EventEmitter } from 'events';
+import { app, protocol, BrowserWindow } from 'electron';
+import { createProtocol, installVueDevtools } from 'vue-cli-plugin-electron-builder/lib';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const webpackDevURL: string | undefined = process.env.WEBPACK_DEV_SERVER_URL;
+export const ee = new EventEmitter();
+
+let win: BrowserWindow | null;
+
+/**
+ * Register schemes as privileged, need to be called before the `ready` event
+ * is fired.
+ */
+export function registerSchemes() {
+  protocol.registerSchemesAsPrivileged([
+    {
+      scheme: 'app',
+      privileges: {
+        secure: true,
+        standard: true
+      }
+    }
+  ]);
+}
+
+/**
+ * Create a new `BrowserWindow`, erase the current window.
+ */
+export function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  if (webpackDevURL) {
+    win.loadURL(webpackDevURL);
+  } else {
+    createProtocol('app');
+    win.loadURL('app://./index.html');
+  }
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+/**
+ * Register various Electron events such as `ready`. Also send a `ready` event
+ * to the local event-emitter.
+ */
+export function registerEvents() {
+  app.on('ready', async () => {
+    if (isDevelopment && !process.env.IS_TEST) {
+      try {
+        await installVueDevtools();
+      } catch (e) {
+        console.error('Vue Devtools failed to install:', e.toString());
+      }
+    }
+
+    createWindow();
+    ee.emit('ready');
+  });
+}
