42a8343f753e0883029ee8af73fd22c3443c2600
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-20 15:15:44 +0200

    chore(app): init vue app class component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
   <div id="app" />
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class App extends Vue {}
+</script>
