284e423368e0872f1be5203450b34d2e571a36d5
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-20 15:21:01 +0200

    tooling(eslint): disable no-extraneous-dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   rules: {
     'class-methods-use-this': 'off',
+    'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
