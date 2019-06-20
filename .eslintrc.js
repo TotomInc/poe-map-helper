057c1169ec07c3b6e99c13a005412d66c45a9e9a
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-20 16:27:33 +0200

    tooling(eslint): prettier clashes with vue/max-attributes-per-line

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,16 @@ module.exports = {
     'class-methods-use-this': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
