8867ad6e2bc061e8715220ad3fa4303bf231c34d
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-20 15:16:12 +0200

    tooling(eslint): disable class-methods-use-this and prefer-default-export

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     ecmaVersion: 2018
   },
   rules: {
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
