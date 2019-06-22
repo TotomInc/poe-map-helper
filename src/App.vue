040a4118b33e2325837b485609b2af9a0a382b8c
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-22 19:29:23 +0200

    feat(app): add transitions on router-view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app" class="w-screen h-screen bg-dark-discord">
-    <router-view />
+    <transition name="fade" mode="out-in" :duration="300">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -13,6 +15,8 @@ export default class App extends Vue {}
 
 <style lang="postcss">
 @import '../node_modules/@vue/ui/dist/vue-ui.css';
+
 @import 'assets/styles/tailwind.postcss';
 @import 'assets/css/fonts.css';
+@import 'assets/css/transitions.css';
 </style>
