fed32887892064e20409a1b47ba7a7cb46af67e9
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-26 08:43:54 +0200

    feat(models): add POEMapItem interface

--- src/models/PathOfExileAPI.ts
+++ src/models/PathOfExileAPI.ts
@@ -7,3 +7,15 @@ export interface POECharacter {
   level: number;
   name: string;
 }
+
+export interface POEMapItem {
+  name: string;
+  /** -1 is unknown, 0 is normal, 1 is magic, 2 is rare, 3 is unique */
+  rarity: number;
+  modifiers: string[];
+  itemLevel: number;
+  tier: number;
+  iq: number;
+  ir: number;
+  mps: number;
+}
