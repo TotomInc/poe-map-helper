6102debe355ea26377f628a1ecf074e1c64ff6e0
Thomas Cazade <cazade.thomas@gmail.com>
2019-06-26 16:56:55 +0200

    feat(models): add models from poe.watch api

--- src/models/PathOfExileAPI.ts
+++ src/models/PathOfExileAPI.ts
@@ -20,3 +20,40 @@ export interface POELeague {
   start?: Date;
   end?: Date;
 }
+
+/**
+ * Compact data used in `/get` request of `poe.watch` API
+ */
+export interface POEWatchItemCompact {
+  id: number;
+  mean: number;
+  median: number;
+  mode: number;
+  min: number;
+  max: number;
+  exalted: number;
+  total: number;
+  daily: number;
+  current: number;
+  accepted: number;
+}
+
+/**
+ * Item data used in `/get` request of `poe.watch` API
+ */
+export interface POEWatchItemData {
+  id: number;
+  name: string;
+  category: string;
+  group: string;
+  frame: string;
+  icon: string;
+}
+
+/**
+ * Model for a `currency` item from `poe.watch` API
+ */
+export interface POEWatchCurrency extends POEWatchItemData, POEWatchItemCompact {
+  history: number[];
+  stackSize: number;
+}
