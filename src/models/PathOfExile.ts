0a29243a44475d313e5756d8192a29f884c8fb98
Thomas Cazade <cazade.thomas@gmail.com>
2019-07-21 23:55:14 +0200

    feat(zones): add zones conts (hideouts & maps)

--- src/models/PathOfExile.ts
+++ src/models/PathOfExile.ts
@@ -10,7 +10,9 @@ export interface POECharacter {
 
 export interface POEMapItem {
   name: string;
-  /** -1 is unknown, 0 is normal, 1 is magic, 2 is rare, 3 is unique */
+  /**
+   * -1 is unknown, 0 is normal, 1 is magic, 2 is rare, 3 is unique
+   */
   rarity: number;
   modifiers: string[];
   itemLevel: number;
@@ -31,4 +33,13 @@ export interface POELeague {
   challenge: boolean;
   start?: Date;
   end?: Date;
+}
+
+/**
+ * Zone details where the player is actually.
+ */
+export interface POEMapZone {
+  name: string;
+  tier: number;
+  unique: boolean;
 }
