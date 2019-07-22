88836aa2c4d432258fb1c059081ca5355b902d3b
Thomas Cazade <cazade.thomas@gmail.com>
2019-07-22 08:45:28 +0200

    feat(models): add poe stash-item model

--- src/models/PathOfExile.ts
+++ src/models/PathOfExile.ts
@@ -23,6 +23,61 @@ export interface POEMapItem {
 }
 
 /**
+ * An item in a stash-tab.
+ */
+export interface POEStashItem {
+  verified: boolean;
+  w: number;
+  h: number;
+  x: number;
+  y: number;
+  ilvl: number;
+  icon: string;
+  league: string;
+  id: string;
+  inventoryId: string;
+  name: string;
+  typeLine: string;
+  identified: boolean;
+  descrText: string;
+  frameType: number;
+
+  category: {
+    currency?: ['fossil'];
+    weapons?: ['oneaxe', 'twoaxe'];
+    armour?: ['helmet', 'gloves', 'chest', 'boots'];
+    accessories?: ['ring', 'amulet', 'belt'];
+    flasks?: [];
+    gems?: ['activegem', 'supportgem'];
+  };
+
+  sockets?: {
+    group: number;
+    attr: string;
+    sColour: string;
+  }[];
+
+  properties?: {
+    name: string;
+    values: [string, number][];
+    displayMode: number;
+    type?: number;
+  }[];
+
+  requirements?: {
+    name: string;
+    values: [string, number][];
+    displayMode: number;
+  }[];
+
+  support?: boolean;
+  secDescrText?: string;
+  explicitMods?: string[];
+  socketedItems?: POEStashItem[];
+  stackSize?: number;
+}
+
+/**
  * Zone details where the player is actually.
  */
 export interface POEMapZone {
