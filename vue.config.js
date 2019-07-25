6662ee496ea127f9a442c6f42a8d69b5c20c671b
Thomas Cazade <cazade.thomas@gmail.com>
2019-07-25 15:20:08 +0200

    tooling: add webpack-bundle-analyzer

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      // new BundleAnalyzerPlugin({
+      //   analyzerPort: IN_PRODUCTION ? 8889 : 8888
+      // })
+    ]
+  }
+};
