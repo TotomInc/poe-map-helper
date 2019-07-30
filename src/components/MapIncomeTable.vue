9b521bf5ac266b565e1953a2c5653a97fe93b95c
Thomas Cazade <cazade.thomas@gmail.com>
2019-07-30 11:49:26 +0200

    feat(map-income-table): add table sort

--- src/components/MapIncomeTable.vue
+++ src/components/MapIncomeTable.vue
@@ -1,7 +1,57 @@
 <template>
   <div id="map-income-table-component">
+    <div class="flex flex-row px-2 py-1 rounded bg-discord-900">
+      <div class="flex flex-row items-center w-1/2 cursor-pointer" @click="changeSortBy('name')">
+        <p class="text-lg">
+          Item name
+        </p>
+
+        <div class="ml-2 fill-current">
+          <div v-if="sortBy === 'name'">
+            <ArrowDownIcon />
+          </div>
+
+          <div v-else>
+            <ArrowUpIcon />
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-row items-center justify-end w-1/4 cursor-pointer" @click="changeSortBy('chaos')">
+        <p class="text-lg cursor-pointer">
+          Chaos income
+        </p>
+
+        <div class="ml-2 fill-current cursor-pointer">
+          <div v-if="sortBy === 'chaos'">
+            <ArrowDownIcon />
+          </div>
+
+          <div v-else>
+            <ArrowUpIcon />
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-row items-center justify-end w-1/4 cursor-pointer" @click="changeSortBy('exalt')">
+        <p class="text-lg cursor-pointer">
+          Exalt income
+        </p>
+
+        <div class="ml-2 fill-current cursor-pointer">
+          <div v-if="sortBy === 'exalt'">
+            <ArrowDownIcon />
+          </div>
+
+          <div v-else>
+            <ArrowUpIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div
-      v-for="(item, index) in stash.itemsDiffIncome"
+      v-for="(item, index) in sortedItemsDiffIncome"
       :key="'item-' + index"
       class="flex flex-row px-2 py-1 rounded"
       :class="{ 'bg-discord-900': index % 2 === 1 }"
@@ -45,14 +95,24 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import IosArrowDownIcon from 'vue-ionicons/dist/ios-arrow-down.vue';
+import IosArrowUpIcon from 'vue-ionicons/dist/ios-arrow-up.vue';
 
 import { RateState } from '@/store/rate/rate.state';
 import { StashState } from '@/store/stash/stash.state';
 import { stashGetters } from '@/store/stash/stash.consts';
 import { POEWatchCurrency } from '../models/POEWatch';
+import { POEPricedStashItem } from '../models/PathOfExile';
 
-@Component({})
+@Component({
+  components: {
+    ArrowDownIcon: IosArrowDownIcon,
+    ArrowUpIcon: IosArrowUpIcon
+  }
+})
 export default class MapIncomeTableComponent extends Vue {
+  private sortBy: 'name' | 'chaos' | 'exalt' = 'chaos';
+
   get rate(): RateState {
     return this.$store.state.rate;
   }
@@ -65,12 +125,75 @@ export default class MapIncomeTableComponent extends Vue {
     return this.$store.getters[stashGetters.getTotalItemsDiffIncome];
   }
 
+  get sortedItemsDiffIncome(): POEPricedStashItem[] {
+    const { itemsDiffIncome } = this.stash;
+    const frozenItemsDiffIncome = itemsDiffIncome.map((item) => Object.freeze(item));
+
+    let sortedItems: POEPricedStashItem[] = [];
+
+    if (this.sortBy === 'name') {
+      sortedItems = this.sortAlphabetically(frozenItemsDiffIncome);
+    } else if (this.sortBy === 'chaos') {
+      sortedItems = this.sortChaos(frozenItemsDiffIncome);
+    } else if (this.sortBy === 'exalt') {
+      sortedItems = this.sortExalt(frozenItemsDiffIncome);
+    }
+
+    return sortedItems;
+  }
+
   private cleanIconURL(iconURL: string): string {
     const url = new URL(iconURL);
 
     url.search = '';
 
     return url.href;
+  }
+
+  private changeSortBy(type: 'name' | 'chaos' | 'exalt') {
+    this.sortBy = type;
+  }
+
+  private sortAlphabetically(items: POEPricedStashItem[]): POEPricedStashItem[] {
+    return items.sort((a, b) => {
+      if (a.typeLine < b.typeLine) {
+        return -1;
+      }
+
+      if (a.typeLine > b.typeLine) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  private sortChaos(items: POEPricedStashItem[]): POEPricedStashItem[] {
+    return items.sort((a, b) => {
+      if (a.chaos < b.chaos) {
+        return 1;
+      }
+
+      if (a.chaos > b.chaos) {
+        return -1;
+      }
+
+      return 0;
+    });
+  }
+
+  private sortExalt(items: POEPricedStashItem[]): POEPricedStashItem[] {
+    return items.sort((a, b) => {
+      if (a.exalt < b.exalt) {
+        return 1;
+      }
+
+      if (a.exalt > b.exalt) {
+        return -1;
+      }
+
+      return 0;
+    });
   }
 }
 </script>
