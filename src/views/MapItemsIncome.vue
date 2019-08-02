66aa428c4aefdc2eb76860715cef6439f1640caa
Thomas Cazade <cazade.thomas@gmail.com>
2019-08-02 10:36:12 +0200

    feat(map-items-income): add map-income-table

--- src/views/MapItemsIncome.vue
+++ src/views/MapItemsIncome.vue
@@ -14,16 +14,36 @@
     <h1 class="text-gray-300 text-center text-4xl mb-4 select-none">
       Map income report
     </h1>
+
+    <div class="max-w-2xl mx-auto p-4 rounded text-discord-100 bg-discord-700 shadow-2xl select-none">
+      <map-income-table :items-diff-income="mapItemsIncome" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 
-@Component({})
+import { POEPricedStashItem } from '@/models/PathOfExile';
+import { MapState } from '@/store/map/map.state';
+import MapIncomeTable from '@/components/MapIncomeTable.vue';
+
+@Component({
+  components: {
+    MapIncomeTable
+  }
+})
 export default class mapItemsIncomeView extends Vue {
-  get mapIndex(): string | undefined {
-    return this.$route.params.id;
+  get mapParamIndex(): number {
+    return parseInt(this.$route.params.id, 10);
+  }
+
+  get map(): MapState {
+    return this.$store.state.map;
+  }
+
+  get mapItemsIncome(): POEPricedStashItem[] {
+    return this.map.mapsHistory[this.mapParamIndex].items;
   }
 
   public goBack() {
