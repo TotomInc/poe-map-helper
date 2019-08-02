82b1d0c6d98447b63d5276ee1bec27b7ce9e9e78
Thomas Cazade <cazade.thomas@gmail.com>
2019-08-02 08:41:44 +0200

    feat(mapping-history): add vue-good-table

--- src/views/MappingHistory.vue
+++ src/views/MappingHistory.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="mapping-history-view" class="relative container mx-auto py-8">
     <div
-      class="back-home absolute inline-flex text-discord-100 py-1 px-3 rounded-full bg-discord-500 cursor-pointer"
+      class="back-home absolute inline-flex text-gray-300 py-1 px-3 rounded-full bg-discord-500 cursor-pointer"
       @click="goToHome()"
     >
       <i class="material-icons mr-2">
@@ -14,18 +14,62 @@
     <h1 class="text-gray-300 text-center text-4xl mb-4 select-none">
       Mapping history
     </h1>
+
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      :pagination-options="{
+        enabled: true,
+        perPage: 10,
+        perPageDropdown: [5, 10, 20],
+        dropdownAllowAll: false,
+        rowsPerPageLabel: 'Maps per page'
+      }"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'map.name'" class="flex flex-row items-center">
+          <img :src="getMapIconURL(props.row.map)" class="w-8 h-8 mr-2" />
+          <span>{{ props.row.map.name }}</span>
+        </span>
+      </template>
+    </vue-good-table>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Mixins } from 'vue-property-decorator';
 
+import POEMapIconURLMixin from '@/mixins/POEMapIconURL';
 import { MapState } from '@/store/map/map.state';
+import { POEMapItem } from '@/models/PathOfExile';
+import { rawMapsImageURL } from '../consts/zones';
 
 @Component({})
-export default class MappingHistoryView extends Vue {
+export default class MappingHistoryView extends Mixins(POEMapIconURLMixin) {
+  private columns = [
+    {
+      label: 'Map',
+      field: 'map.name',
+      sortable: false
+    },
+    {
+      label: 'Chaos',
+      field: 'income.chaos',
+      type: 'decimal'
+    },
+    {
+      label: 'Exalt',
+      field: 'income.exalt',
+      type: 'decimal'
+    }
+  ];
+
   get map(): MapState {
     return this.$store.state.map;
+  }
+
+  get rows() {
+    return this.map.mapsHistory;
   }
 
   public goToHome() {
