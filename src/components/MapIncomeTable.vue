45c75e7f8286d727ff4307edd4fc49d1ed45b57b
Thomas Cazade <cazade.thomas@gmail.com>
2019-08-05 19:07:58 +0200

    refacto(map-income-table): use vue-good-table

--- src/components/MapIncomeTable.vue
+++ src/components/MapIncomeTable.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="map-income-table-component">
-    <div
-      v-for="(item, index) in itemsDiffIncome"
-      :key="'item-' + index"
-      class="flex flex-row px-2 py-1 rounded"
-      :class="{ 'bg-discord-900': index % 2 === 1 }"
+    <vue-good-table
+      ref="vgt"
+      :columns="columns"
+      :rows="itemsDiffIncome"
+      :pagination-options="{
+        enabled: true,
+        perPage: 10,
+        perPageDropdown: [10, 20, 40],
+        dropdownAllowAll: false,
+        rowsPerPageLabel: 'Items per page'
+      }"
+      :sort-options="{
+        initialSortBy: {
+          field: 'chaos',
+          type: 'desc'
+        }
+      }"
+      style-class="vgt-table striped"
     >
-      <div class="flex flex-row items-center w-1/2">
-        <img :src="cleanIconURL(item.icon)" class="h-6 mr-1" />
-        <p>
-          {{ item.typeLine }}
-          <span class="text-vue-500">x{{ item.stackSize ? item.stackSize : 1 }}</span>
-        </p>
-      </div>
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'typeLine'" class="flex flex-row items-center">
+          <img :src="cleanIconURL(props.row.icon)" class="h-6 mr-1" />
+          <span>{{ props.row.typeLine }}</span>
+        </span>
 
-      <div class="flex flex-row items-center justify-end w-1/4">
-        <p>{{ item.chaos }}</p>
-        <img :src="require('@/assets/images/orbs/chaos-orb.png')" class="h-6 ml-1" />
-      </div>
+        <span v-if="props.column.field == 'chaos'" class="flex flex-row items-center float-right">
+          <span class="mr-2">{{ props.row.chaos }}</span>
+          <img :src="require('@/assets/images/orbs/chaos-orb.png')" class="w-6 h-6" />
+        </span>
 
-      <div class="flex flex-row items-center justify-end w-1/4">
-        <p>{{ item.exalt }}</p>
-        <img :src="require('@/assets/images/orbs/exalted-orb.png')" class="h-6 ml-1" />
-      </div>
-    </div>
+        <span v-if="props.column.field == 'exalt'" class="flex flex-row items-center float-right">
+          <span class="mr-2">{{ props.row.exalt }}</span>
+          <img :src="require('@/assets/images/orbs/exalted-orb.png')" class="w-6 h-6" />
+        </span>
+      </template>
+    </vue-good-table>
 
     <div class="flex flex-row px-2 py-1 rounded">
       <div class="flex flex-row items-center w-1/2">
@@ -53,6 +65,24 @@ import { RateState } from '@/store/rate/rate.state';
 @Component({})
 export default class MapIncomeTableComponent extends Vue {
   @Prop() readonly itemsDiffIncome!: POEPricedStashItem[];
+
+  private columns = [
+    {
+      label: 'Item name',
+      field: 'typeLine',
+      sortable: false
+    },
+    {
+      label: 'Chaos income',
+      field: 'chaos',
+      type: 'decimal'
+    },
+    {
+      label: 'Exalt income',
+      field: 'exalt',
+      type: 'decimal'
+    }
+  ];
 
   get rate(): RateState {
     return this.$store.state.rate;
