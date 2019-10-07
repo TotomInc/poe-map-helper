c7c534096535ef3217a844933a16670d9ddeb212
Thomas Cazade <cazade.thomas@gmail.com>
2019-10-07 13:33:39 +0200

    feat(app): browser view, refacto splash-screen as component

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="splash-screen-component" class="h-full w-full z-0 pointer-events-none">
+    <div class="splash-screen z-0" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class SplashScreenComponent extends Vue {}
+</script>
+
+<style>
+#splash-screen-component {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.splash-screen {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  opacity: 0.1;
+  background-image: url('../assets/images/legion-splash.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
