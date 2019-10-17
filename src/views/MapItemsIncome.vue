69be7d5a82ed9a9e495e86b62336a92c454dfb99
Thomas Cazade <cazade.thomas@gmail.com>
2019-10-17 08:19:28 +0200

    feat(map-items-income): add components animation

--- src/views/MapItemsIncome.vue
+++ src/views/MapItemsIncome.vue
@@ -2,13 +2,16 @@
   <div id="maps-items-income-view" class="relative container mx-auto py-8">
     <back-button :label="'Back'" @on-click="goBack" />
 
-    <h1 class="text-gray-300 text-center text-4xl mb-4 select-none">
+    <h1 class="text-gray-300 text-center text-4xl mb-16 select-none">
       Map income report
     </h1>
 
-    <map-details :map="mapHistory.map" :character="poeSelectedCharacter" class="mb-4" />
+    <map-details ref="mapDetails" :map="mapHistory.map" :character="poeSelectedCharacter" class="mb-4 opacity-0" />
 
-    <div class="max-w-2xl mx-auto p-4 rounded text-discord-100 bg-discord-700 shadow-2xl select-none">
+    <div
+      ref="mapIncomeTable"
+      class="max-w-2xl mx-auto p-4 rounded text-discord-100 bg-discord-700 shadow-2xl select-none opacity-0"
+    >
       <map-income-table :items-diff-income="mapHistory.items" />
     </div>
   </div>
@@ -50,6 +53,56 @@ export default class MapItemsIncomeView extends Vue {
 
   public goBack() {
     this.$router.back();
+  }
+
+  public mounted(): void {
+    this.animateAppearingComponents();
+  }
+
+  public beforeDestroy(): void {
+    this.animateDiseappearingComponents();
+  }
+
+  /**
+   * Animate appearing components, translateX from bottom to top with opacity
+   * from 0 to 1.
+   */
+  private animateAppearingComponents() {
+    const mapDetailsComp = this.$refs.mapDetails as Vue;
+    const mapIncomeTable = this.$refs.mapIncomeTable as HTMLDivElement;
+
+    const targets = [mapDetailsComp.$el, mapIncomeTable];
+
+    const animation = anime({
+      targets,
+      easing: 'spring(2, 100, 100, 0)',
+      delay: anime.stagger(250),
+      translateY: -50,
+      opacity: 1,
+    });
+
+    animation.play();
+  }
+
+  /**
+   * Animate disappearing components, translate from top to bottom with opacity
+   * from 1 to 0.
+   */
+  private animateDiseappearingComponents() {
+    const mapDetailsComp = this.$refs.mapDetails as Vue;
+    const mapIncomeTable = this.$refs.mapIncomeTable as HTMLDivElement;
+
+    const targets = [mapDetailsComp.$el, mapIncomeTable];
+
+    const animation = anime({
+      targets,
+      easing: 'spring(1, 100, 100, 0)',
+      delay: anime.stagger(100),
+      translateY: 0,
+      opacity: 0,
+    });
+
+    animation.play();
   }
 }
 </script>
